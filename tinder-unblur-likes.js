b32b41b03a4eda78916fcf8e8ef59037e0c74cb5
Harshit <pm.mahendra1@gmail.com>
2023-05-02 00:25:20 +0200

    refactor code

--- tinder-unblur-likes.js
+++ tinder-unblur-likes.js
@@ -1,16 +1,22 @@
 async function unblur() {
-    const teasers = await fetch("https://api.gotinder.com/v2/fast-match/teasers", { "headers": { "X-Auth-Token": localStorage.getItem('TinderWeb/APIToken'), platform: 'android' }}).then(res => res.json()).then(res => res.data.results);
-    const teaserEls = document.querySelectorAll('.Expand.enterAnimationContainer > div:nth-child(1)');
-    
-    for (let i = 0; i < teaserEls.length; ++i) {
-      const teaser = teasers[i];
-      const teaserEl = teaserEls[i];
-      
-      const teaserImage = `https://preview.gotinder.com/${teaser.user._id}/original_${teaser.user.photos[0].id}.jpeg`;
-        
-        console.log(teaser.user.photos);
-      
-      teaserEl.style.backgroundImage = `url(${teaserImage})`;
-    }
-  }
-  unblur();
+  const teasers = await fetch("https://api.gotinder.com/v2/fast-match/teasers", {
+    headers: {
+      "X-Auth-Token": localStorage.getItem("TinderWeb/APIToken"),
+      platform: "android",
+    },
+  })
+    .then((res) => res.json())
+    .then((res) => res.data.results);
+
+  const teaserEls = document.querySelectorAll(
+    ".Expand.enterAnimationContainer > div:nth-child(1)"
+  );
+
+  teasers.forEach((teaser, index) => {
+    const teaserEl = teaserEls[index];
+    const teaserImage = `https://preview.gotinder.com/${teaser.user._id}/original_${teaser.user.photos[0].id}.jpeg`;
+    teaserEl.style.backgroundImage = `url(${teaserImage})`;
+  });
+}
+
+unblur();
